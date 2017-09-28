4dcc21f960b9a007e387c035f562ff6b36b66127
MachDomo <dominic.c.ma@gmail.com>
2017-09-28 12:06:58 -0700

    "initialized hello world, server and webpack"

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const
+  path = require('path'),
+  webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  devtool: 'inline-source-map',
+  watch: true,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['env', 'react'] },
+        }],
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react',
+      ReactDOM: 'react-dom'
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: false,
+    port: 3000
+  }
+};
