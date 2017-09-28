92330a51143b8d0c2237b11fe9b3256460dbee56
Dominic Ma <dominic.c.ma@gmail.com>
2017-09-28 12:35:14 -0700

    Reorganized folder chain for client and public

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static((__dirname + '/public')));
+app.use(express.static((__dirname + '/src/public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/src/public/index.html');
 })
 
 
