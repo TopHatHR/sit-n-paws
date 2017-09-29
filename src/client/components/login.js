daa2739e4bb5556fbce6547e85db5bc0706f72fd
Edward Plato <edplatomail@gmail.com>
2017-09-29 12:20:37 -0700

    Began testing material-ui popup modal login

--- src/client/components/login.js
+++ src/client/components/login.js
@@ -1,21 +1,94 @@
 import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 // login form that takes username and password
 
-const Login = (props) => {
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
+    this.state = {
+      open: true,
+      username: '',
+      password: '',
+    }
+
+    this.handleOpen = () => {
+      this.setState({open: true});
+    }
+
+    this.handleClose = () => {
+      this.setState({open: false});
+    }
+
+    this.handleSubmit = () => {
+      console.log('SUBMITTED');
+      // this.setState({open: false});
+    }
+
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onClick={() => {
+          this.handleSubmit();
+          this.handleClose();
+        }}
+      />,
+    ];
+
+    return (
       <div>
-        <h1>LOGIN</h1>
-        <form>
-          <label>username</label>
-          <input type="text" />
-          <label>password</label>
-          <input type="password" />
-          <input type="submit" value="login" />
-        </form>
+        <RaisedButton label="Dialog" onClick={this.handleOpen} />
+        <Dialog
+          title="Dialog With Actions"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          <div>
+            <h1>LOGIN</h1>
+            <form>
+              <label>username</label>
+              <input type="text" />
+              <label>password</label>
+              <input type="password" />
+              <input type="submit" value="login" />
+            </form>
+          </div>
+        </Dialog>
       </div>
-    )
+    );
+  }
 }
 
-export default Login;
+
+// const Login = (props) => {
+
+//   return (
+//       <div>
+//         <h1>LOGIN</h1>
+//         <form>
+//           <label>username</label>
+//           <input type="text" />
+//           <label>password</label>
+//           <input type="password" />
+//           <input type="submit" value="login" />
+//         </form>
+//       </div>
+//     )
+// }
+
+// export default Login;
