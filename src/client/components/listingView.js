4e17b24a108a08836850e13abf4c8733ede7fb53
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-04 12:00:04 -0700

    Added Cards to ListingsView

--- src/client/components/listingView.js
+++ src/client/components/listingView.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 const ListingView = (props) => (
   <div>
-    <h1>Listing:</h1>
-    <p>Name: Joe</p>
-    <p>Zip: 91524</p>
-    <p>Dog Size Preference: Medium or Under</p>
-    <p>dogTemperatmentPreference: Mean</p>
-    <p>dogActivityPreference: Lazy</p>
-    <p>homeAttributes: Open Spaces</p>
-    <p>hostPictures: </p>
-    <p>homePictures: </p>
+    <Card>
+      <CardHeader
+        title="Joe"
+        subtitle="Puppy Lover in 91524"
+      />
+      <CardMedia
+        overlay={<CardTitle title="$30 Per Night!" subtitle="Premium Kibble Included." />}
+      >
+        <img src="http://diy.sndimg.com/content/dam/images/diy/fullset/2010/5/10/0/CI-Nathaniel-Riethmann_Doghouse_s4x3.jpg.rend.hgtvcom.966.725.suffix/1420697544779.jpeg" alt="" />
+      </CardMedia>
 
-    <p>cost: $30</p>
+      <CardTitle title="Friendly dog lover just looking to make some spare cash!" subtitle="Small or Medium dogs only!" />
+      <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+      </CardText>
+
+
+
+      <CardActions>
+        <FlatButton label="Contact Me" />
+        <FlatButton label="Learn more about this host!" />
+    </CardActions>
+    </Card>
   </div>
 
 
