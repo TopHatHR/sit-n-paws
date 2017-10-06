24d82b5eda558cfda03c1541c6fd1a360eeea3e6
Lillian Lee <lillianlee118@gmail.com>
2017-10-05 18:09:04 -0700

    changes

--- src/client/components/main.js
+++ src/client/components/main.js
@@ -6,13 +6,15 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 import exampleListingData from '../../public/MOCK_DATA.js';
+import Search from './search.js'
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      listings: exampleListingData,
+      listings: [],
+      query: '',
       openDrawer: false,
     }
 
@@ -20,9 +22,24 @@ export default class Main extends React.Component {
       this.setState({openDrawer: !this.state.openDrawer});
     }
 
+
+    this.get = (query) => {
+    var url = 'http://localhost:3000/listings';
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        // if (data.zipcode === )
+        this.setState({listings: data});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    };
+
+    this.getListings = (query) => {
+      this.get(query);
+    }
   }
-
-
 
   render() {
     return (
@@ -30,7 +47,7 @@ export default class Main extends React.Component {
       <div>
         <AppBar showMenuIconButton={false} iconElementRight={<IconButton><NavigationMenu/></IconButton>} onRightIconButtonTouchTap={this.touchTap} />
         <h1>MAIN COMPONENT</h1>
-
+        <Search onClick={this.getListings.bind(this)}/>
         // drawer
 
         // partial profile
