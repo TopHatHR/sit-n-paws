48fe02a4d1bc979e1a22c6ee2b240d8a1e0a5b8c
Lillian Lee <lillianlee118@gmail.com>
2017-10-06 18:07:06 -0700

    changes

--- src/client/components/listingsContainer.js
+++ src/client/components/listingsContainer.js
@@ -12,7 +12,6 @@ export default class ListingsContainer extends React.Component {
     return (
       <div className="wrapper">
         {this.props.listings.map((listing, i) => {
-          if (i < 10) {
             return (
               <ListingView listing={listing} key={listing.name} />
             )
