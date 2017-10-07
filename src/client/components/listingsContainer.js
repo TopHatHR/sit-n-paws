78f1945b25d150744fb28ff01c22b59079b2f208
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-06 17:09:22 -0700

    Removed cap of 10 on listings

--- src/client/components/listingsContainer.js
+++ src/client/components/listingsContainer.js
@@ -12,11 +12,21 @@ export default class ListingsContainer extends React.Component {
     return (
       <div className="wrapper">
         {this.props.listings.map((listing, i) => {
+<<<<<<< 1cb806e5fa20dad5a5b9fe4d8d0245577dc8fa84
             return (
               <ListingView listing={listing} key={listing.name} />
             )
           }
         )}
+=======
+
+
+            return (
+              <ListingView listing={listing} key={listing.name} />
+            )
+
+        })}
+>>>>>>> Removed cap of 10 on listings
       </div>
     );
   };
