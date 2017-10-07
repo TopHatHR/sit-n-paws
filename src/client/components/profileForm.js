a151fb614fe26e6330082d1da3a1d1d61ba1d059
Chris Pfaff <chrispfaff10@gmail.com>
2017-10-06 20:33:13 -0400

    updateddrawer component

--- src/client/components/profileForm.js
+++ src/client/components/profileForm.js
@@ -10,46 +10,58 @@ export default class ProfileUpdate extends React.Component {
     super(props);
 
     this.state = {
-      Email: '',
-      Name: '',
-      Phone: '',
-      Address: ''
+      email: '',
+      name: '',
+      phone: '',
+      address: '',
+      renderState: false,
     }
 
     this.handleEmailChange = (event) => {
       event.preventDefault();
-      this.setState({Email: event.target.value});
+      this.setState({email: event.target.value});
     }
 
     this.handleNameChange = (event) => {
       event.preventDefault();
-      this.setState({Name: event.target.value});
+      this.setState({name: event.target.value});
     }
 
 
     this.handlePhoneChange = (event) => {
       event.preventDefault();
-      this.setState({Phone: event.target.value});
+      this.setState({phone: event.target.value});
     }
 
     this.handleAddressChange = (event) => {
       event.preventDefault();
-      this.setState({Address: event.target.value});
+      this.setState({address: event.target.value});
     }
 
     this.handleSubmit = (event) => {
       this.updateProfile(this.state);
+      this.setState({renderState: !this.state.renderState})
     }
 
     this.updateProfile = (query) => {
+      console.log(query)
       var url = 'http://localhost:3000/profile';
-      LoginSubmit(url, query, (res) => {
-        console.log('RES: ', res);
-        if(res.success === true) {
-          console.log('true');
-        } else {
-          console.log(res.error);
-        }
+      var options = {
+        method: 'POST',
+        body: JSON.stringify(query),
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      };
+
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log('DATA: ', data);
+          callback(data);
+        })
+        .catch((errors) => {
+          console.log('Login Error: ', errors);
       })
     }
   }
@@ -62,17 +74,17 @@ export default class ProfileUpdate extends React.Component {
         <fieldset>
           <legend>Enter Your Email</legend>
           <br />
-          <input type="text" value={this.state.Email} onChange={this.handleEmailChange}/>
+          <input type="text" value={this.state.email} onChange={this.handleEmailChange}/>
           <br />
           <br />
           <label>Name:</label>
-          <input type="text" value={this.state.Name} onChange={this.handleNameChange}/>
+          <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
           <br />
           <label>Phone:</label>
-          <input type="text" value={this.state.Phone} onChange={this.handlePhoneChange}/>
+          <input type="text" value={this.state.phone} onChange={this.handlePhoneChange}/>
           <br />
           <label>Address:</label>
-          <input type="text" value={this.state.Address} onChange={this.handleAddressChange}/>
+          <input type="text" value={this.state.address} onChange={this.handleAddressChange}/>
           </fieldset>
         </form>
         <br />
