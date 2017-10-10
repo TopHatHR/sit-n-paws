781e38583ebeaea081952164a8b4a651c2fe2f41
Edward Plato <edplatomail@gmail.com>
2017-10-09 18:37:27 -0700

    Comment, clean up seed.js, tests, component utils

--- seed.js
+++ seed.js
@@ -1,21 +1,26 @@
+// seed.js is for seeding the database with mock data to work with in the interface
+// this function is invoked near the top of the server.js file and can be easily commented out to stop seeding
+
 // import mongoose, config, and listing schema
 const mongoose = require('mongoose');
 const Listing = require('./db/models/listing');
 const User = require('./db/models/users');
-// seed data
+
+// seed data for listings to populate host listings
 const listingsData = [
-{"name":"Chris Pfaff","zipcode":94106,"description":"great home with lots of space","dogSizePreference":"medium","dogBreedPreference":"Corgi","email":"chrispfaff10@gmail.com","dogActivityPreference":"rutrum","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/44.jpg","homePictures":"https://farm7.staticflickr.com/6076/6080657644_19cfe82456.jpg","cost":35},
-{"name":"Niels Larson","zipcode":94110,"description":"Great home with lots of space","dogSizePreference":"super extra large","dogBreedPreference":"Chihuahua","email":"nlarson94@gmail.com","dogActivityPreference":"dapibus","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/45.jpg","homePictures":"https://farm1.staticflickr.com/68/187943195_05de9fe99b.jpg","cost":55},
-{"name":"Thomasina Luscombe","zipcode":94123,"description":"Great home with lots of space","dogSizePreference":"small","dogBreedPreference":"Dachshund","email":"hi@gmail.com","dogActivityPreference":"lacus","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/46.jpg","homePictures":"https://farm6.staticflickr.com/5510/14490433662_2745930345.jpg","cost":30},
-{"name":"Shelley Philpot","zipcode":94106,"description":"Great home with lots of space","dogSizePreference":"teeny weeny","dogBreedPreference":"German Shepherd","email":"hi@gmail.com","dogActivityPreference":"amet","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/47.jpg","homePictures":"https://farm4.staticflickr.com/3062/3046570389_f960000e36.jpg","cost":65},
-{"name":"Isidora Hemms","zipcode":94110,"description":"Great home with lots of space","dogSizePreference":"large","dogBreedPreference":"Pitbull","email":"hi@gmail.com","dogActivityPreference":"ipsum","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/48.jpg","homePictures":"https://farm1.staticflickr.com/229/516113751_e2222a5a64.jpg","cost":30},
-{"name":"Say Swinglehurst","zipcode":94123,"description":"Great home with lots of space","dogSizePreference":"small","dogBreedPreference":"ROSIE","email":"hi@gmail.com","dogActivityPreference":"fusce","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/men/55.jpg","homePictures":"https://farm1.staticflickr.com/48/111317752_7934d93e8a.jpg","cost":57},
-{"name":"Angus Bafford","zipcode":94106,"description":"Great home with lots of space","dogSizePreference":"medium","dogBreedPreference":"CHUNKY","email":"hi@gmail.com","dogActivityPreference":"lorem","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/men/56.jpg","homePictures":"https://scontent.fsjc1-3.fna.fbcdn.net/v/t31.0-8/14500780_151623151960598_8947738040944491792_o.jpg?oh=c2362b13fe7e7e25c1b1c0cfc5319147&oe=5A7FDE71","cost":60},
-{"name":"Breanne Carnoghan","zipcode":94110,"description":"Great home with lots of space","dogSizePreference":"medium","dogBreedPreference":"Bloodhound","email":"hi@gmail.com","dogActivityPreference":"erat","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/men/57.jpg","homePictures":"https://farm4.staticflickr.com/3586/3468872496_d62d4580b9.jpg","cost":32},
-{"name":"Fabio Handaside","zipcode":94123,"description":"Great home with lots of space","dogSizePreference":"small","dogBreedPreference":"Mix","email":"hi@gmail.com","dogActivityPreference":"proin","homeAttributes":"Great home with lots of space", "hostPictures":"https://randomuser.me/api/portraits/men/58.jpg","homePictures":"https://farm4.staticflickr.com/3193/2683030380_8ac4712010.jpg","cost":25},
-{"name":"Lily Feake","zipcode":94106,"description":"Great home with lots of space","dogSizePreference":"medium","dogBreedPreference":"All","email":"hi@gmail.com","dogActivityPreference":"vel","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/men/59.jpg","homePictures":"https://farm4.staticflickr.com/3163/2780745441_a39b974e55.jpg","cost":55}
+{"name":"Chris Pfaff","zipcode":94106,"dogSizePreference":"medium","dogBreedPreference":"Corgi","email":"chrispfaff10@gmail.com","dogActivityPreference":"rutrum","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/44.jpg","homePictures":"https://farm7.staticflickr.com/6076/6080657644_19cfe82456.jpg","cost":35},
+{"name":"Niels Larson","zipcode":94110,"dogSizePreference":"super extra large","dogBreedPreference":"Chihuahua","email":"nlarson94@gmail.com","dogActivityPreference":"dapibus","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/45.jpg","homePictures":"https://farm1.staticflickr.com/68/187943195_05de9fe99b.jpg","cost":55},
+{"name":"Thomasina Luscombe","zipcode":94123,"dogSizePreference":"small","dogBreedPreference":"Dachshund","email":"hi1@gmail.com","dogActivityPreference":"lacus","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/46.jpg","homePictures":"https://farm6.staticflickr.com/5510/14490433662_2745930345.jpg","cost":30},
+{"name":"Shelley Philpot","zipcode":94106,"dogSizePreference":"teeny weeny","dogBreedPreference":"German Shepherd","email":"hi2@gmail.com","dogActivityPreference":"amet","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/47.jpg","homePictures":"https://farm4.staticflickr.com/3062/3046570389_f960000e36.jpg","cost":65},
+{"name":"Isidora Hemms","zipcode":94110,"dogSizePreference":"large","dogBreedPreference":"Pitbull","email":"hi3@gmail.com","dogActivityPreference":"ipsum","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/women/48.jpg","homePictures":"https://farm1.staticflickr.com/229/516113751_e2222a5a64.jpg","cost":30},
+{"name":"Say Swinglehurst","zipcode":94123,"dogSizePreference":"small","dogBreedPreference":"ROSIE","email":"hi4@gmail.com","dogActivityPreference":"fusce","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/men/55.jpg","homePictures":"https://farm1.staticflickr.com/48/111317752_7934d93e8a.jpg","cost":57},
+{"name":"Angus Bafford","zipcode":94106,"dogSizePreference":"medium","dogBreedPreference":"CHUNKY","email":"hi5@gmail.com","dogActivityPreference":"lorem","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/men/56.jpg","homePictures":"https://scontent.fsjc1-3.fna.fbcdn.net/v/t31.0-8/14500780_151623151960598_8947738040944491792_o.jpg?oh=c2362b13fe7e7e25c1b1c0cfc5319147&oe=5A7FDE71","cost":60},
+{"name":"Breanne Carnoghan","zipcode":94110,"dogSizePreference":"medium","dogBreedPreference":"Bloodhound","email":"hi@gmail.com","dogActivityPreference":"erat","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/men/57.jpg","homePictures":"https://farm4.staticflickr.com/3586/3468872496_d62d4580b9.jpg","cost":32},
+{"name":"Fabio Handaside","zipcode":94123,"dogSizePreference":"small","dogBreedPreference":"Mix","email":"hi6@gmail.com","dogActivityPreference":"proin","homeAttributes":"Great home with lots of space", "hostPictures":"https://randomuser.me/api/portraits/men/58.jpg","homePictures":"https://farm4.staticflickr.com/3193/2683030380_8ac4712010.jpg","cost":25},
+{"name":"Lily Feake","zipcode":94106,"dogSizePreference":"medium","dogBreedPreference":"All","email":"hi7@gmail.com","dogActivityPreference":"vel","homeAttributes":"Great home with lots of space","hostPictures":"https://randomuser.me/api/portraits/men/59.jpg","homePictures":"https://farm4.staticflickr.com/3163/2780745441_a39b974e55.jpg","cost":55}
 ];
 
+// seed data for user to provide instant login capability
 const mockCompleteUser = [
   {
     username: 'mary444',
@@ -27,30 +32,27 @@ const mockCompleteUser = [
   }
 ];
 
-// function to clean listings from database and seed with above mockData
+// function to clean listings from database and seed with above listings and user
 const seedListingDB = () => {
-  // remove listings to start
+  // remove listings from database to start - NOTE THIS REMOVES ALL LISTINGS, SO BE CAREFUL IN PRODUCTION
   Listing.remove({}, (err) => {
     if(err) {
       console.log(err);
     } else {
-      console.log('DATABASE SEED INIT: REMOVED PREVIOUS LISTINGS');
-      // remove mary444 user from mock data above to prevent db collisions
+      // remove mary444 user prior to adding
       User.remove({'username': 'mary444'}, (err) => {
         if(err) {
           console.log(err);
         }
         let reformatUser = JSON.stringify(mockCompleteUser[0]);
         let newUser = new User(JSON.parse(reformatUser));
-        // add mock data mary444
+        // add user mary444 to database
         newUser.save((err) => {
           if(err) {
             console.log(err);
-          } else {
-            console.log('DATABASE SEED: ADDED MOCK USER');
           }
         })
-        // iterate over mock listings, format, and save into db
+        // iterate over mock listings, format, and save each listing into the database
         listingsData.forEach((listing) => {
           // reformat data to strings for parsing before saving
           let reformatListing = JSON.stringify(listing);
@@ -62,7 +64,7 @@ const seedListingDB = () => {
           })
         })
       })
-      console.log('DATABASE SEED: ADDED MOCK LISTINGS');
+      console.log('DATABASE SEEDED');
     }
   })
 }
--- src/client/utils/submitListing.js
+++ src/client/utils/submitListing.js
@@ -1,27 +1,22 @@
 export default function submitListing(url, formData, callback) {
 
   // post request for user login with user credentials object
-
+  // the formData will be a FormData object
+  // identical to utils/login.js, but without the headers
+  // used in PostListing.js
   var options = {
     method: 'POST',
-    body: formData,
-    // headers: new Headers({
-    //   'Content-Type': 'multipart/form-data'
-    // })
+    body: formData
   };
 
-  console.log('POST SUBMITTED', formData);
+  // this console.log is valuable when working with creating listings
+  // console.log('POST SUBMITTED, formData');
   fetch(url, options)
     .then((res) => res.json())
     .then((data) => {
-
-      console.log('DATA: ', data);
-
-
       callback(data);
     })
     .catch((errors) => {
       console.log('Login Error: ', errors);
     })
-
 }
