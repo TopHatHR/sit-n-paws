a48d6a50642e099a43b6124c6dbb0fdbf331188f
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-09 17:38:05 -0700

    Commented private.js

--- src/client/components/private.js
+++ src/client/components/private.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+// Redirects to login if checkAuth returns false, else allows rendering of Main
 const PrivateRoute = ({ component: Component, checkAuth }) => (
   <Route render={(props) => (
     checkAuth() ? (
