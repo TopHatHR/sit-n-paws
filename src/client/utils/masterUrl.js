b53063cad061c94934cc9fa5752b132743d65cfe
Zachary Biernat <zackbiernat@gmail.com>
2017-10-11 18:59:01 -0700

    Added env variables

--- src/client/utils/masterUrl.js
+++ src/client/utils/masterUrl.js
@@ -1,6 +1,6 @@
 // masterUrl that can be changed depending on your server port
 // used in listingView.js, login.js, main.js, PostListing.js
 
-const masterUrl = "http://localhost:3000";
+const masterUrl = process.env.HEROKU_URL + process.env.PORT || "http://localhost:3000";
 
 export default masterUrl;
